c25ba85c7125204b7e1613b2fe93e7618b68edce
Chris <chriscombs@vaclaims.net>
2022-01-07 04:25:39 +0000

    d

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,50 +1,41 @@
-<script setup lang="ts">
-const name = ref('')
+<template>
+    <input
+        id="input"
+        v-model="q"
+        placeholder="Search"
+        type="text"
+        autocomplete="false"
+        p="x-4 y-2"
+        w="250px"
+        text="center"
+        bg="transparent"
+        border="~ rounded gray-200 dark:gray-700"
+        outline="none active:none"
+        @keydown.enter="getx"
+    />
+    <div>
+        <button :disabled="!q" class="m-3 text-sm btn" @click="getx">Go</button>
+    </div>
+</template>
 
-const router = useRouter()
-const go = () => {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            q: ''
+        }
+    },
+    methods: {
+        async getx() {
+            let { data } = await axios.get('https://goo.iamlyrics.com/api/xnxxs/blonde')
+            console.log(data);
+        }
+    },
+    mounted() {
+        console.log('mounted');
+    },
 }
 </script>
-
-<template>
-  <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
-    </div>
-  </div>
-</template>
